fd01f7a4ce1f1616b3d4ff2b82f233db332f32a1
Ivan <gyunderov@gmail.com>
2022-11-13 15:04:17 +0200

    HaskellRecurssion

--- HaskelUrok2/SecondApp.hs
+++ HaskelUrok2/SecondApp.hs
@@ -7,7 +7,12 @@ repeatString str n =
     if n == 0
         then ""
         else str ++ (repeatString str (n-1))
-        
+
+repeatStringLoop string result n = 
+    if n == 0
+        then result 
+        else repeatStringLoop string (result ++ string) (n -1)      
 main :: IO()
 main = do
-    print(repeatString "niz" 100000)
+    
+    print(repeatString "niz" 10000000000)
